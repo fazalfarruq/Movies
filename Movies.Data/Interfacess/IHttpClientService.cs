a7cee090acd1f9fa21336768b86b397850c28f3c
FazalFarruq <fazal.farruq@fiserv.com>
2021-05-21 01:07:20 -0400

    Add project files.

---
+++ Movies.Data/Interfacess/IHttpClientService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movies.Data.Interfacess
+{
+    public interface IHttpClientService
+    {
+        Task<T> Get<T>(string requestUri);
+    }
+}
---
+++ Movies.Service/HttpClientService.cs
@@ -0,0 +1,35 @@
+﻿using Movies.Data.Interfacess;
+using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Movies.Service
+{
+    public class HttpClientService : IHttpClientService
+    {
+        public async Task<T> Get<T>(string requestUri)
+        {
+            try
+            {
+                using (HttpClient httpClient = new HttpClient())
+                {
+                    var result = await httpClient.GetAsync(requestUri);
+
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var responseFromServer = await result.Content.ReadAsStringAsync();
+                        var response = JsonConvert.DeserializeObject<T>(responseFromServer);
+                        return response;
+                    }
+                }
+
+                return default;
+            }
+            catch (Exception e)
+            {
+                return default;
+            }
+        }
+    }
+}
